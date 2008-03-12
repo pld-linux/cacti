b938103bb903518c8ca40838b6c18ee6f681a113
Elan Ruusamäe <glen@pld-linux.org>
2008-03-12 00:36:29 +0000

    - saved from .spec
    Changed files:
        cacti.cfg.php -> 1.1

---
+++ cacti.cfg.php
@@ -0,0 +1,20 @@
+<?php
+$database_type = 'mysql';
+$database_default = 'cacti';
+$database_hostname = 'localhost';
+$database_username = 'cactiuser';
+$database_password = 'cactiuser';
+
+$plugins = array();
+// $plugins[] = 'thold';
+// $plugins[] = 'monitor';
+// $plugins[] = 'discovery';
+
+/* Do not edit this line */
+$config = array();
+
+/* This is full URL Path to the Cacti installation
+   For example, if your cacti was accessible by http://server/cacti/ you would user '/cacti/'
+   as the url path.  For just http://server/ use '/'
+*/
+$config['url_path'] = '/cacti/';
